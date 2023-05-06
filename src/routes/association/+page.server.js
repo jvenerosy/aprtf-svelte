e9a69c246947d164bc155a0cb5edda938437f0fb
jveneros <jveneros@bouyguestelecom.fr>
2023-05-06 02:41:33 +0200

    🚧 test vercel

--- src/routes/association/+page.server.js
+++ src/routes/association/+page.server.js
@@ -3,6 +3,6 @@ import { PUBLIC_HOST_API } from '$env/static/public';
 /** @type {import('./$types').LayoutServerLoad} */
 export function load() {
     return {
-        develop: PUBLIC_HOST_API
+        deploymentGitBranch: "develop"
     };
 }
