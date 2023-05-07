50319d90433f0afb1f321994ff4276b3b00a3f75
jveneros <jveneros@bouyguestelecom.fr>
2023-05-07 16:55:30 +0200

    test wh

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -115,6 +115,7 @@
                                                 name="raison"
                                             >
                                                 <option value="" disabled selected hidden>La raison de votre contact</option>
+                                                
                                                 <option value="1">Raison 1</option>
                                                 <option value="2">Raison 2</option>
                                                 <option value="3">Raison 3</option>
