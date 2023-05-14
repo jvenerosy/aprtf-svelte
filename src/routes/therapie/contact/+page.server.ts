5880cc7ab49a1f1b15a050f3a992f135757a224d
jveneros <jveneros@bouyguestelecom.fr>
2023-05-14 18:56:31 +0200

    🚧 WIP label therapie

--- src/routes/therapie/contact/+page.server.ts
+++ src/routes/therapie/contact/+page.server.ts
@@ -2,6 +2,13 @@
 /** @type {import('./$types').Actions} */
 export const actions = {
     step1: async ({request}) => {
-        console.log('proute');
+        return {
+            step: 2
+        };
+    },
+    step2: async ({request}) => {
+        return {
+            step: 3
+        };
     }
 };
